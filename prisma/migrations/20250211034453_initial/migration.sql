4e74835cba0a66d08ef07b586db928cdf7b3e019
Brad Garropy <bradgarropy@gmail.com>
2025-02-10 21:45:47 -0600

    add created and updated columns.

--- prisma/migrations/20250211034453_initial/migration.sql
+++ prisma/migrations/20250211034453_initial/migration.sql
@@ -2,7 +2,9 @@
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateIndex
