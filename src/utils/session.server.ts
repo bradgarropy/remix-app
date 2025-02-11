4d70aaed3c4d4ca1965cd7cca9a05f51a1c98a68
Brad Garropy <bradgarropy@gmail.com>
2025-02-10 22:31:49 -0600

    🔐 authentication (#6)

---
+++ src/utils/session.server.ts
@@ -0,0 +1,63 @@
+import type {User} from "@prisma/client"
+import {createCookieSessionStorage, redirect} from "@remix-run/node"
+
+import {getUserById} from "~/utils/users.server"
+
+type UserSession = {
+    userId: User["id"]
+}
+
+const sessionStorage = createCookieSessionStorage<UserSession>({
+    cookie: {
+        name: "__session",
+        httpOnly: true,
+        path: "/",
+        sameSite: "lax",
+        secrets: [process.env.SESSION_SECRET!],
+        secure: process.env.NODE_ENV === "production",
+    },
+})
+
+const getSession = async (request: Request) => {
+    const cookie = request.headers.get("Cookie")
+    const session = await sessionStorage.getSession(cookie)
+
+    return session
+}
+
+type CreateSessionParams = {
+    request: Request
+    userId: User["id"]
+}
+
+const createSession = async ({request, userId}: CreateSessionParams) => {
+    const session = await getSession(request)
+    session.set("userId", userId)
+
+    const cookie = await sessionStorage.commitSession(session, {
+        // https://github.com/remix-run/indie-stack/blob/main/app/session.server.ts#L66
+    })
+
+    return redirect("/dashboard", {headers: {"set-cookie": cookie}})
+}
+
+const deleteSession = async (request: Request) => {
+    const session = await getSession(request)
+    const cookie = await sessionStorage.destroySession(session)
+
+    return redirect("/", {headers: {"set-cookie": cookie}})
+}
+
+const getUserFromSession = async (request: Request) => {
+    const session = await getSession(request)
+    const userId = session.get("userId")
+
+    if (!userId) {
+        return null
+    }
+
+    const user = await getUserById(userId)
+    return user
+}
+
+export {createSession, deleteSession, getSession, getUserFromSession}
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import {sentryVitePlugin as sentry} from "@sentry/vite-plugin"
 import tailwind from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import {remixDevTools} from "remix-development-tools"
+import {loadEnv} from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import {defineConfig} from "vitest/config"
 
@@ -17,73 +18,76 @@ declare module "@remix-run/node" {
     }
 }
 
-const config = defineConfig({
-    build: {
-        sourcemap: true,
-    },
-    plugins: [
-        tsconfigPaths(),
-        tailwind(),
-        remixDevTools({
-            client: {
-                showBreakpointIndicator: false,
-            },
-        }),
-        process.env.VITEST
-            ? react()
-            : remix({
-                  appDirectory: "src",
-                  ignoredRouteFiles: ["**/.*"],
-                  future: {
-                      v3_fetcherPersist: true,
-                      v3_relativeSplatPath: true,
-                      v3_throwAbortReason: true,
-                      v3_lazyRouteDiscovery: true,
-                      v3_singleFetch: true,
-                      v3_routeConfig: true,
-                  },
-                  serverModuleFormat: "esm",
-              }),
-        process.env.SENTRY_AUTH_TOKEN
-            ? sentry({
-                  authToken: process.env.SENTRY_AUTH_TOKEN,
-                  org: process.env.SENTRY_ORG,
-                  project: process.env.SENTRY_PROJECT,
-                  release: {
-                      create: true,
-                      name: createRelease(),
-                  },
-                  sourcemaps: {
-                      filesToDeleteAfterUpload: [
-                          "build/client/**/*.map",
-                          "build/server/**/*.map",
-                      ],
-                  },
-                  telemetry: false,
-              })
-            : null,
-    ],
-    server: {
-        open: true,
-        port: 3000,
-    },
-    test: {
-        clearMocks: true,
-        coverage: {
-            all: false,
-            clean: true,
-            enabled: true,
-            provider: "v8",
-            reporter: ["text", "lcov", "html"],
-            reportOnFailure: false,
+const config = defineConfig(({mode}) => {
+    return {
+        build: {
+            sourcemap: true,
         },
-        environment: "jsdom",
-        include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
-        globals: false,
-        passWithNoTests: true,
-        setupFiles: "src/tests/setup.ts",
-        watch: false,
-    },
+        plugins: [
+            tsconfigPaths(),
+            tailwind(),
+            remixDevTools({
+                client: {
+                    showBreakpointIndicator: false,
+                },
+            }),
+            process.env.VITEST
+                ? react()
+                : remix({
+                      appDirectory: "src",
+                      ignoredRouteFiles: ["**/.*"],
+                      future: {
+                          v3_fetcherPersist: true,
+                          v3_relativeSplatPath: true,
+                          v3_throwAbortReason: true,
+                          v3_lazyRouteDiscovery: true,
+                          v3_singleFetch: true,
+                          v3_routeConfig: true,
+                      },
+                      serverModuleFormat: "esm",
+                  }),
+            process.env.SENTRY_AUTH_TOKEN
+                ? sentry({
+                      authToken: process.env.SENTRY_AUTH_TOKEN,
+                      org: process.env.SENTRY_ORG,
+                      project: process.env.SENTRY_PROJECT,
+                      release: {
+                          create: true,
+                          name: createRelease(),
+                      },
+                      sourcemaps: {
+                          filesToDeleteAfterUpload: [
+                              "build/client/**/*.map",
+                              "build/server/**/*.map",
+                          ],
+                      },
+                      telemetry: false,
+                  })
+                : null,
+        ],
+        server: {
+            open: true,
+            port: 3000,
+        },
+        test: {
+            clearMocks: true,
+            coverage: {
+                all: false,
+                clean: true,
+                enabled: true,
+                provider: "v8",
+                reporter: ["text", "lcov", "html"],
+                reportOnFailure: false,
+            },
+            env: loadEnv(mode, process.cwd(), ""),
+            environment: "jsdom",
+            include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+            globals: false,
+            passWithNoTests: true,
+            setupFiles: "src/tests/setup.ts",
+            watch: false,
+        },
+    }
 })
 
 export default config
