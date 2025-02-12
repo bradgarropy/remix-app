f7b22f6d7a73d2b832162968fe88c6271ef6ade6
Brad Garropy <bradgarropy@gmail.com>
2025-02-12 08:58:15 -0600

    add authentication test.

---
+++ src/e2e/auth.spec.ts
@@ -0,0 +1,53 @@
+import {expect, test} from "@playwright/test"
+
+test("sign up", async ({page}) => {
+    await page.goto("http://localhost:3000")
+    await page.getByRole("link", {name: "Sign up"}).click()
+
+    await page.getByRole("textbox", {name: "Email"}).fill("marge@gmail.com")
+
+    await page
+        .getByRole("textbox", {name: "Password", exact: true})
+        .fill("password")
+
+    await page
+        .getByRole("textbox", {name: "Confirm password", exact: true})
+        .fill("password")
+
+    await page.getByRole("button", {name: "Sign up"}).click()
+
+    await expect(page.getByText("Hello marge@gmail.com")).toBeVisible()
+})
+
+test("sign in", async ({page}) => {
+    await page.goto("http://localhost:3000")
+    await page.getByRole("link", {name: "Sign in"}).click()
+
+    await page.getByRole("textbox", {name: "Email"}).fill("homer@gmail.com")
+    await page.getByRole("textbox", {name: "Password"}).fill("password")
+    await page.getByRole("button", {name: "Sign in"}).click()
+
+    await expect(page.getByText("Hello homer@gmail.com")).toBeVisible()
+})
+
+test("sign out", async ({page}) => {
+    await page.goto("http://localhost:3000")
+    await page.getByRole("link", {name: "Sign in"}).click()
+
+    await page.getByRole("textbox", {name: "Email"}).fill("homer@gmail.com")
+    await page.getByRole("textbox", {name: "Password"}).fill("password")
+    await page.getByRole("button", {name: "Sign in"}).click()
+
+    await expect(page.getByText("Hello homer@gmail.com")).toBeVisible()
+
+    await page.getByRole("link", {name: "Sign out"}).click()
+
+    await expect(page.getByRole("heading", {name: "Home"})).toBeVisible()
+})
+
+test("authenticated content", async ({page}) => {
+    await page.goto("http://localhost:3000")
+    await page.getByRole("link", {name: "Dashboard"}).click()
+
+    await expect(page.getByRole("heading", {name: "Sign in"})).toBeVisible()
+})
