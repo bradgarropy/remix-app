4ebaf62bdcdb7070793fa477d5ae9a184b7e71d8
Brad Garropy <bradgarropy@gmail.com>
2025-02-11 21:13:55 -0600

    improve navigation tests.

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,10 +1,17 @@
 import {render, screen} from "@testing-library/react"
 import {MemoryRouter} from "react-router-dom"
-import {expect, test} from "vitest"
+import {expect, test, vitest} from "vitest"
+
+import * as auth from "~/utils/auth.server"
+
+const useAuthSpy = vitest.spyOn(auth, "useAuth")
 
 import Navigation from "~/components/Navigation"
+import {mockUser} from "~/mocks/users"
 
-test("renders", () => {
+test("shows unauthenticated routes", () => {
+    useAuthSpy.mockReturnValueOnce(null)
+
     render(
         <MemoryRouter>
             <Navigation />
@@ -13,4 +20,27 @@ test("renders", () => {
 
     expect(screen.getByText("Home")).toBeInTheDocument()
     expect(screen.getByText("About")).toBeInTheDocument()
+    expect(screen.getByText("Dashboard")).toBeInTheDocument()
+    expect(screen.getByText("Sign up")).toBeInTheDocument()
+    expect(screen.getByText("Sign in")).toBeInTheDocument()
+    expect(screen.queryByText("Sign out")).not.toBeInTheDocument()
+    expect(screen.getByText("Sentry")).toBeInTheDocument()
+})
+
+test("shows authenticated routes", () => {
+    useAuthSpy.mockReturnValueOnce(mockUser)
+
+    render(
+        <MemoryRouter>
+            <Navigation />
+        </MemoryRouter>,
+    )
+
+    expect(screen.getByText("Home")).toBeInTheDocument()
+    expect(screen.getByText("About")).toBeInTheDocument()
+    expect(screen.getByText("Dashboard")).toBeInTheDocument()
+    expect(screen.queryByText("Sign up")).not.toBeInTheDocument()
+    expect(screen.queryByText("Sign in")).not.toBeInTheDocument()
+    expect(screen.getByText("Sign out")).toBeInTheDocument()
+    expect(screen.getByText("Sentry")).toBeInTheDocument()
 })
