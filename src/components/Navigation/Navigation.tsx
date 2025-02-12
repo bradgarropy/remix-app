56f0516dee6765d033e217d85c0c2c216603532b
Brad Garropy <bradgarropy@gmail.com>
2025-02-11 21:02:48 -0600

    show and hide navigation items based on authentication status.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,10 @@
 import {NavLink} from "@remix-run/react"
 
+import {useAuth} from "~/utils/auth.server"
+
 const Navigation = () => {
+    const user = useAuth()
+
     return (
         <nav className="flex gap-4 font-bold">
             <NavLink to="/" prefetch="intent">
@@ -15,17 +19,21 @@ const Navigation = () => {
                 Dashboard
             </NavLink>
 
-            <NavLink to="/signup" prefetch="intent">
-                Sign up
-            </NavLink>
+            {user ? (
+                <NavLink to="/signout" prefetch="none">
+                    Sign out
+                </NavLink>
+            ) : (
+                <>
+                    <NavLink to="/signup" prefetch="intent">
+                        Sign up
+                    </NavLink>
 
-            <NavLink to="/signin" prefetch="intent">
-                Sign in
-            </NavLink>
-
-            <NavLink to="/signout" prefetch="none">
-                Sign out
-            </NavLink>
+                    <NavLink to="/signin" prefetch="intent">
+                        Sign in
+                    </NavLink>
+                </>
+            )}
 
             <NavLink to="/sentry" prefetch="intent">
                 Sentry
