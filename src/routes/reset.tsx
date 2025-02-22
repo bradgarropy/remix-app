ba165e8da0a98860019fc4dba2a014fd96253e9d
Brad Garropy <bradgarropy@gmail.com>
2025-02-21 21:44:48 -0600

    add reset password flow.

--- src/routes/reset.tsx
+++ src/routes/reset.tsx
@@ -1,10 +1,7 @@
-import crypto from "node:crypto"
-
 import type {ActionFunctionArgs, MetaFunction} from "@remix-run/node"
-import {Form, Link, useActionData} from "@remix-run/react"
+import {Form} from "@remix-run/react"
 
-import {createReset} from "~/utils/resets"
-import {getUserByEmail} from "~/utils/users.server"
+import {resetPassword} from "~/utils/auth.server"
 
 export const meta: MetaFunction = () => [
     {
@@ -12,39 +9,53 @@ export const meta: MetaFunction = () => [
     },
 ]
 
-export const action = async ({request}: ActionFunctionArgs) => {
-    const formData = await request.formData()
-
-    const email = String(formData.get("email"))
-    const user = await getUserByEmail(email)
-
-    if (!user) {
-        throw new Error("User not found")
+export const action = async ({request, params}: ActionFunctionArgs) => {
+    if (!params.token) {
+        throw new Error("No password reset token provided")
     }
 
-    const token = crypto.randomUUID()
-    await createReset({userId: user.id, token})
+    const formData = await request.formData()
 
-    return {token}
+    const newPassword = String(formData.get("newPassword"))
+
+    const newPasswordConfirmation = String(
+        formData.get("newPasswordConfirmation"),
+    )
+
+    return resetPassword({
+        request,
+        token: params.token,
+        newPassword,
+        newPasswordConfirmation,
+    })
 }
 
 const Route = () => {
-    const data = useActionData<typeof action>()
-
     return (
         <>
             <h2 className="text-2xl font-bold mb-8">Reset password</h2>
 
-            <Form method="post" className="grid max-w-xs mb-8">
-                <label htmlFor="email">Email</label>
+            <Form method="post" className="grid max-w-xs">
+                <label htmlFor="newPassword">New password</label>
                 <input
                     required
-                    id="email"
-                    name="email"
-                    type="email"
+                    id="newPassword"
+                    name="newPassword"
+                    type="password"
+                    autoComplete="new-password"
                     className="px-4 py-2 border-2 border-black rounded-md mb-4"
                 />
-
+                <label htmlFor="newPasswordConfirmation">
+                    Confirm new password
+                </label>{" "}
+                <input
+                    required
+                    id="newPasswordConfirmation"
+                    name="newPasswordConfirmation"
+                    type="password"
+                    autoComplete="new-password"
+                    className="px-4 py-2 border-2 border-black rounded-md mb-4"
+                />
                 <button
                     type="submit"
                     className="bg-black text-white px-4 py-2 rounded-md font-bold hover:bg-purple-500"
@@ -52,15 +63,6 @@ const Route = () => {
                     Reset
                 </button>
             </Form>
-
-            {data?.token ? (
-                <Link
-                    to={`/reset/token/${data.token}`}
-                    className="hover:text-purple-500"
-                >
-                    Click this link to reset your password
-                </Link>
-            ) : null}
         </>
     )
 }
