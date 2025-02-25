aaa6b65c1b371bd2eac147423eb055f8a925d196
Brad Garropy <bradgarropy@gmail.com>
2025-02-25 08:36:53 -0600

    add get note and update note tests.

--- src/models/notes.server.test.ts
+++ src/models/notes.server.test.ts
@@ -3,7 +3,7 @@ import {describe, expect, test} from "vitest"
 import {mockDb} from "~/mocks/db"
 import {mockNote, mockNotes} from "~/mocks/notes"
 import {homer} from "~/mocks/users"
-import {createNote, getNotes} from "~/models/notes.server"
+import {createNote, getNote, getNotes, updateNote} from "~/models/notes.server"
 
 describe("getNotes", () => {
     test("gets notes", async () => {
@@ -14,11 +14,34 @@ describe("getNotes", () => {
     })
 })
 
+describe("getNote", () => {
+    test("gets note", async () => {
+        mockDb.note.findUnique.mockResolvedValueOnce(mockNote)
+
+        const notes = await getNote({id: mockNote.id, userId: homer.id})
+        expect(notes).toEqual(mockNote)
+    })
+})
+
 describe("createNote", () => {
     test("creates note", async () => {
         mockDb.note.create.mockResolvedValueOnce(mockNote)
 
         const note = await createNote(mockNote)
+        expect(note).toEqual(mockNote)
+    })
+})
+
+describe("updateNote", () => {
+    test("updates note", async () => {
+        mockDb.note.update.mockResolvedValueOnce(mockNote)
+
+        const note = await updateNote({
+            id: mockNote.id,
+            userId: homer.id,
+            content: "Updated note",
+        })
+
         expect(note).toEqual(mockNote)
     })
 })
