1c9383a4026058ed3e1dc77a69a030064fafd610
Brad Garropy <bradgarropy@gmail.com>
2025-02-24 22:00:00 -0600

    seed database with mock notes.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,12 @@
+import {mockNotes} from "~/mocks/notes"
 import {mockUser} from "~/mocks/users"
+import {createNote} from "~/models/notes.server"
 import {createUser} from "~/models/users.server"
 
 await createUser(mockUser)
+
+const mockNotesPromises = mockNotes.map(note =>
+    createNote({userId: note.userId, content: note.content}),
+)
+
+await Promise.all(mockNotesPromises)
