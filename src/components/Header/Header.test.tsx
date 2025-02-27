c94c9abeeb1bf3828ec4d1ea0b74704fdb89f632
Brad Garropy <bradgarropy@gmail.com>
2025-02-27 08:30:19 -0600

    show user email.

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -4,10 +4,14 @@ import {expect, test, vitest} from "vitest"
 
 import Header from "~/components/Header"
 import * as Navigation from "~/components/Navigation"
+import {homer} from "~/mocks/users"
+import * as auth from "~/utils/auth"
 
+const useAuthSpy = vitest.spyOn(auth, "useAuth")
 const NavigationSpy = vitest.spyOn(Navigation, "default")
 
-test("shows header", () => {
+test("shows header with user", () => {
+    useAuthSpy.mockReturnValueOnce(homer)
     NavigationSpy.mockReturnValueOnce(<p>navigation</p>)
 
     render(
@@ -17,4 +21,19 @@ test("shows header", () => {
     )
 
     expect(screen.getByText("remix app")).toBeInTheDocument()
+    expect(screen.getByText(homer.email)).toBeInTheDocument()
+})
+
+test("shows header without user", () => {
+    useAuthSpy.mockReturnValueOnce(null)
+    NavigationSpy.mockReturnValueOnce(<p>navigation</p>)
+
+    render(
+        <MemoryRouter>
+            <Header />
+        </MemoryRouter>,
+    )
+
+    expect(screen.getByText("remix app")).toBeInTheDocument()
+    expect(screen.queryByText(homer.email)).not.toBeInTheDocument()
 })
