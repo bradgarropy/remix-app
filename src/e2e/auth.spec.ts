eec27e657af9bf799bd6402abdb7026ff1f11acc
Brad Garropy <bradgarropy@gmail.com>
2025-03-06 21:21:07 -0600

    fix auth and navigation tests.

--- src/e2e/auth.spec.ts
+++ src/e2e/auth.spec.ts
@@ -1,10 +1,12 @@
 import {expect, test} from "@playwright/test"
 
+import {homer} from "~/mocks/users"
+
 test("sign up", async ({page}) => {
-    await page.goto("http://localhost:3000")
+    await page.goto("/")
     await page.getByRole("link", {name: "Sign up"}).click()
 
-    await page.getByRole("textbox", {name: "Email"}).fill("marge@gmail.com")
+    await page.getByRole("textbox", {name: "Email"}).fill("bart@gmail.com")
 
     await page
         .getByRole("textbox", {name: "Password", exact: true})
@@ -16,39 +18,53 @@ test("sign up", async ({page}) => {
 
     await page.getByRole("button", {name: "Sign up"}).click()
 
-    await expect(page.getByText("Home")).toBeVisible()
+    await expect(page.getByText("bart@gmail.com")).toBeVisible()
+    await expect(page.getByRole("heading", {name: "Home"})).toBeVisible()
 })
 
 test("sign in", async ({page}) => {
-    await page.goto("http://localhost:3000")
+    await page.goto("/")
     await page.getByRole("link", {name: "Sign in"}).click()
 
-    await page.getByRole("textbox", {name: "Email"}).fill("homer@gmail.com")
-    await page.getByRole("textbox", {name: "Password"}).fill("password")
+    await page.getByRole("textbox", {name: "Email"}).fill(homer.email)
+    await page.getByRole("textbox", {name: "Password"}).fill(homer.password)
     await page.getByRole("button", {name: "Sign in"}).click()
 
+    await expect(page.getByText(homer.email)).toBeVisible()
     await expect(page.getByRole("heading", {name: "Home"})).toBeVisible()
     await expect(page.getByRole("link", {name: "Sign out"})).toBeVisible()
 })
 
 test("sign out", async ({page}) => {
-    await page.goto("http://localhost:3000")
+    await page.goto("/")
     await page.getByRole("link", {name: "Sign in"}).click()
 
-    await page.getByRole("textbox", {name: "Email"}).fill("homer@gmail.com")
-    await page.getByRole("textbox", {name: "Password"}).fill("password")
+    await page.getByRole("textbox", {name: "Email"}).fill(homer.email)
+    await page.getByRole("textbox", {name: "Password"}).fill(homer.password)
     await page.getByRole("button", {name: "Sign in"}).click()
 
-    await expect(page.getByText("Home")).toBeVisible()
+    await expect(page.getByText(homer.email)).toBeVisible()
 
     await page.getByRole("link", {name: "Sign out"}).click()
 
     await expect(page.getByRole("heading", {name: "Home"})).toBeVisible()
+    await expect(page.getByText(homer.email)).not.toBeVisible()
 })
 
-test("authenticated content", async ({page}) => {
-    await page.goto("http://localhost:3000")
-    await page.getByRole("link", {name: "Dashboard"}).click()
-
+test("authorization", async ({page}) => {
+    await page.goto("/notes")
     await expect(page.getByRole("heading", {name: "Sign in"})).toBeVisible()
+})
+
+test("redirection", async ({page}) => {
+    await page.goto("/notes")
+    await expect(page.getByRole("heading", {name: "Sign in"})).toBeVisible()
+
+    await page.getByRole("textbox", {name: "Email"}).fill(homer.email)
+    await page.getByRole("textbox", {name: "Password"}).fill(homer.password)
+    await page.getByRole("button", {name: "Sign in"}).click()
+
+    await expect(page.getByText(homer.email)).toBeVisible()
+    await expect(page.getByRole("heading", {name: "Notes"})).toBeVisible()
+    await expect(page.getByRole("link", {name: "Sign out"})).toBeVisible()
 })
--- src/e2e/navigation.spec.ts
+++ src/e2e/navigation.spec.ts
@@ -1,20 +1,10 @@
 import {expect, test} from "@playwright/test"
 
 test("unauthenticated", async ({page}) => {
-    await page.goto("http://localhost:3000")
+    await page.goto("/")
 
     await expect(page).toHaveTitle("💿 remix app | home")
     await expect(page.getByRole("heading", {name: "Home"})).toBeVisible()
-
-    await page.getByRole("link", {name: "About"}).click()
-
-    await expect(page).toHaveTitle("💿 remix app | about")
-    await expect(page.getByRole("heading", {name: "About"})).toBeVisible()
-
-    await page.getByRole("link", {name: "Dashboard"}).click()
-
-    await expect(page).toHaveTitle("💿 remix app | sign in")
-    await expect(page.getByRole("heading", {name: "Sign in"})).toBeVisible()
 
     await page.getByRole("link", {name: "Sign up"}).click()
 
@@ -33,29 +23,20 @@ test("unauthenticated", async ({page}) => {
 })
 
 test("authenticated", async ({page}) => {
-    await page.goto("http://localhost:3000")
+    await page.goto("/")
     await page.getByRole("link", {name: "Sign in"}).click()
 
     await page.getByRole("textbox", {name: "Email"}).fill("homer@gmail.com")
     await page.getByRole("textbox", {name: "Password"}).fill("password")
     await page.getByRole("button", {name: "Sign in"}).click()
 
-    await expect(page.getByRole("heading", {name: "Home"})).toBeVisible()
-
-    await page.getByRole("link", {name: "Home"}).click()
-
     await expect(page).toHaveTitle("💿 remix app | home")
     await expect(page.getByRole("heading", {name: "Home"})).toBeVisible()
 
-    await page.getByRole("link", {name: "About"}).click()
+    await page.getByRole("link", {name: "Notes"}).click()
 
-    await expect(page).toHaveTitle("💿 remix app | about")
-    await expect(page.getByRole("heading", {name: "About"})).toBeVisible()
-
-    await page.getByRole("link", {name: "Dashboard"}).click()
-
-    await expect(page).toHaveTitle("💿 remix app | dashboard")
-    await expect(page.getByRole("heading", {name: "Dashboard"})).toBeVisible()
+    await expect(page).toHaveTitle("💿 remix app | notes")
+    await expect(page.getByRole("heading", {name: "Notes"})).toBeVisible()
 
     await page.getByRole("link", {name: "Sentry"}).click()
 
