171affdb7d894e904034d72c63c192ae54819380
Brad Garropy <bradgarropy@gmail.com>
2025-03-07 20:43:12 -0600

    no retries.

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ const config = defineConfig({
     testDir: "./src/e2e",
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
+    retries: 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: "html",
     use: {
