34acf1b48a6480a511c041da1e3eda0eaa638f9e
Brad Garropy <bradgarropy@gmail.com>
2025-04-21 11:13:08 -0500

    🤷🏼‍♂️ forgot password (#21)

--- src/utils/auth.server.ts
+++ src/utils/auth.server.ts
@@ -2,13 +2,18 @@ import type {ResetToken, User} from "@prisma/client"
 import {redirect} from "@remix-run/node"
 import bcrypt from "bcryptjs"
 
-import {deleteResetToken, getResetToken} from "~/models/resetTokens.server"
+import {
+    createResetToken,
+    deleteResetToken,
+    getResetToken,
+} from "~/models/resetTokens.server"
 import {
     createUser,
     getUserByEmail,
     getUserById,
     updatePassword,
 } from "~/models/users.server"
+import {sendEmail} from "~/utils/email.server"
 import {
     createSession,
     deleteSession,
@@ -111,6 +116,34 @@ const requireUser = async (request: Request) => {
     return user
 }
 
+type ForgotPasswordParams = {
+    request: Request
+    email: User["email"]
+}
+
+const forgotPassword = async ({request, email}: ForgotPasswordParams) => {
+    const user = await getUserByEmail(email)
+
+    if (!user) {
+        throw new Error("User not found")
+    }
+
+    const token = await createResetToken({userId: user.id})
+
+    const {origin} = new URL(request.url)
+    const url = `${origin}/reset/${token}`
+
+    const message = await sendEmail({
+        to: user.email,
+        from: "Remix App <remix-app@gmail.com>",
+        subject: "Reset your password",
+        text: `Click this link to reset your password: ${url}`,
+        html: `<p>Click <a href="${url}">this link</a> to reset your password.</p>`,
+    })
+
+    return {message}
+}
+
 type ResetPasswordParams = {
     request: Request
     token: ResetToken["token"]
@@ -162,4 +195,4 @@ const resetPassword = async ({
     })
 }
 
-export {requireUser, resetPassword, signIn, signOut, signUp}
+export {forgotPassword, requireUser, resetPassword, signIn, signOut, signUp}
