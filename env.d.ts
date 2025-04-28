02754605bf8f3c048fd217a04cb3bbaba90bd35c
Brad Garropy <bradgarropy@gmail.com>
2025-04-28 12:42:29 -0500

    add environment variable types.

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,17 @@
 /// <reference types="vite/client" />
 /// <reference types="@remix-run/node" />
+
+interface ViteTypeOptions {
+    strictImportMetaEnv: unknown
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+    readonly VITE_SESSION_SECRET: string
+    readonly VITE_SMTP_USERNAME: string
+    readonly VITE_SMTP_PASSWORD: string
+    readonly VITE_SENTRY_DSN: string
+}
